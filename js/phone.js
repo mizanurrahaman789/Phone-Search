2b9d6e351635d184e7c6171e5db43ed42cab38fc
Your Namemizanurrahaman789 <you@example.com>
2022-02-28 17:06:48 -0700

    phone commit

--- js/phone.js
+++ js/phone.js
@@ -13,20 +13,40 @@ const showPhoneDetails = (phones) => {
             const div = document.createElement('div')
             div.innerHTML = `<div class="card p-5">
       <div class="pro-img">
-            <img class="w-25" src="${phone.image}" alt="">
+            <img class="w-50" src="${phone.image}" alt="">
 
       </div>
-      <h5>Phone Name:${phone.phone_name} </h5>
-      <h5>Release Date: </h5>
-      <h5>Main Features: </h5>
+      <h3>Phone Name: ${phone.phone_name} </h3>
+      <h5>Brand: ${phone.brand} </h5>
+      
+      
       <div class="allbutton">
-            <button class="btn btn-success">Details</button>
+            <button onclick="details('${phone.slug}')" class="btn btn-success">Details</button>
       </div>
 </div>
       
       `;
             parent.appendChild(div)
-            console.log(phone)
+            // console.log(phone)
       }
 
+}
+
+const details = (id) => {
+      const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+      fetch(url)
+            .then(res => res.json())
+            .then(data => setdetails(data.data.mainFeatures))
+      // console.log(url)
+};
+
+const setdetails = (info) => {
+      document.getElementById('details-container').innerHTML = `<div>
+      // <img src="" alt="">
+      <h5>name: ${info.phone_name}</h5>
+      <h5>releaseDate: ${info.releaseDate}</h5>
+      
+      </div>
+      `;
+      console.log(info)
 }
