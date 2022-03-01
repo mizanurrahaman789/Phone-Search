1acf61a2a45a19c25b5e64ce481afd678c6d52cb
Your Namemizanurrahaman789 <you@example.com>
2022-03-01 01:48:48 -0700

    add commit

--- js/phone.js
+++ js/phone.js
@@ -7,16 +7,20 @@ const allphone = () => {
       // console.log(url)
 }
 
+// card section
 const showPhoneDetails = (phones) => {
       for (const phone of phones) {
             const parent = document.getElementById('phone-container')
             const div = document.createElement('div')
-            div.innerHTML = `<div class="card p-5">
+            div.innerHTML = `<div class=""container>
+            <div class="row m-2 text-center">
+            <div class="col-md-4 ">
+            <div class="card p-5 ">
       <div class="pro-img">
             <img class="w-50" src="${phone.image}" alt="">
 
       </div>
-      <h3>Phone Name: ${phone.phone_name} </h3>
+      <h4>Phone Name: ${phone.phone_name} </h4>
       <h5>Brand: ${phone.brand} </h5>
       
       
@@ -24,6 +28,48 @@ const showPhoneDetails = (phones) => {
             <button onclick="details('${phone.slug}')" class="btn btn-success">Details</button>
       </div>
 </div>
+
+            </div>
+
+
+            <div class="col-md-4">
+            <div class="card p-5 ">
+      <div class="pro-img">
+            <img class="w-50" src="${phone.image}" alt="">
+
+      </div>
+      <h4>Phone Name: ${phone.phone_name} </h4>
+      <h5>Brand: ${phone.brand} </h5>
+      
+      
+      <div class="allbutton">
+            <button onclick="details('${phone.slug}')" class="btn btn-success">Details</button>
+      </div>
+</div>
+
+            </div>
+
+
+            <div class="col-md-4 ">
+            <div class="card p-5 ">
+      <div class="pro-img">
+            <img class="w-50" src="${phone.image}" alt="">
+
+      </div>
+      <h4>Phone Name: ${phone.phone_name} </h4>
+      <h5>Brand: ${phone.brand} </h5>
+      
+      
+      <div class="allbutton">
+            <button onclick="details('${phone.slug}')" class="btn btn-success">Details</button>
+      </div>
+</div>
+
+            </div>
+
+            </div>
+            
+            </div>
       
       `;
             parent.appendChild(div)
@@ -31,21 +77,25 @@ const showPhoneDetails = (phones) => {
       }
 
 }
+//second 
 
 const details = (id) => {
       const url = `https://openapi.programming-hero.com/api/phone/${id}`;
       fetch(url)
             .then(res => res.json())
-            .then(data => setdetails(data.data.mainFeatures))
-      // console.log(url)
+            .then(data => setdetails(data.data))
+      console.log(url)
 };
 
 const setdetails = (info) => {
       document.getElementById('details-container').innerHTML = `<div>
-      // <img src="" alt="">
-      <h5>name: ${info.phone_name}</h5>
-      <h5>releaseDate: ${info.releaseDate}</h5>
-      
+      <img class="w-25" src="${info.image}" alt="">
+      <h3>Name: ${info.name}</h3>
+      <h4>Brand: ${info.brand}</h4>
+      <h5>ReleaseDate: ${info.releaseDate}</h5>
+      <h5>Storage: ${info.mainFeatures.storage}</h5>
+      <h5>Storage: ${info.mainFeatures.displaySize}</h5>
+
       </div>
       `;
       console.log(info)
